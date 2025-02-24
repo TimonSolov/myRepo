3dae302e2f09bb44d3864d531a3e3ccc3120549f
TimonSolov <tim.colok@gmail.com>
2025-02-24 18:53:56 +0200

    added

--- lesson_16/locators/main_pages_locators.py
+++
@@ -1,4 +0,0 @@
-USERNAME_FIELD = "//input[@id='user-name']"
-PASSWORD_FIELD_LOCATOR = "//input[@id='password']"
-LOGIN_BUTTON = "//input[@id='login-button']"
-ERROR_MESSAGE = "//div[@class='error-message-container error']"
--- lesson_16/locators/product_pages_locators.py
+++
@@ -1,1 +0,0 @@
-PRODUCT_TILE_LOCATOR = "//div[@class='inventory_list']/div"
