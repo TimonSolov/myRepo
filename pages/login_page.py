ff472acebeda2d0a2931de9d15b8c1a4c37881f5
TimonSolov <tim.colok@gmail.com>
2025-02-24 19:12:18 +0200

    added

---
+++ pages/login_page.py
@@ -0,0 +1,13 @@
+from pages.base_page import BasePage
+from locators.main_pages_locators import *
+
+
+class LoginPage(BasePage):
+
+    def login(self, username: str, password: str):
+        self.fill_text(USERNAME_FIELD, text=username)
+        self.fill_text(locator=PASSWORD_FIELD_LOCATOR, text=password)
+        self.click(locator=LOGIN_BUTTON)
+
+    def get_login_error_message(self):
+        return self.get_text(locator=ERROR_MESSAGE)
---
+++ pages/product_page.py
@@ -0,0 +1,12 @@
+from pages.base_page import BasePage
+from locators.product_pages_locators import *
+
+
+# def find_product_tile(locator: str, count: str):
+#     expect(locator).toHaveCount(count)
+
+
+class ProductPage(BasePage):
+
+    def find_product_tiles(self):
+        return self.page.locator(PRODUCT_TILE_LOCATOR).count()
